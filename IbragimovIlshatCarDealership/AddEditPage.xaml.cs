f02772c2174e32ef222c67b306c65e4f4bd8837e
Ilshatei06 <ibragimoff.ilshat2006@gmail.com>
2025-10-19 17:24:52 +0500

    Добавьте файлы проекта.

---
+++ IbragimovIlshatCarDealership/AddEditPage.xaml.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xaml;
+
+namespace IbragimovIlshatCarDealership
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Car _currentCar = new Car();
+        public AddEditPage(Car SelectedCar)
+        {
+            InitializeComponent();
+
+            if (SelectedCar != null)
+                _currentCar = SelectedCar;
+
+            DataContext = _currentCar;
+
+
+
+            var marks = IbragimovCarDealershipDBEntities.GetContext().Mark.ToList();
+            ComboMark.ItemsSource = marks;
+
+            if (_currentCar.MarkID > 0)
+            {
+                ComboMark.SelectedValue = _currentCar.MarkID;
+            }
+
+            var classes = IbragimovCarDealershipDBEntities.GetContext().Class.ToList();
+            ComboClass.ItemsSource = classes;
+
+            if (_currentCar.ClassID > 0)
+            {
+                ComboClass.SelectedValue = _currentCar.ClassID;
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (ComboMark.SelectedItem == null)
+                errors.AppendLine("Выберите Марку!");
+
+            if (string.IsNullOrWhiteSpace(_currentCar.Model))
+                errors.AppendLine("Укажите Модель!");
+
+            if (_currentCar.Price <= 0)
+                errors.AppendLine("Укажите положительную Цену!");
+
+            if (string.IsNullOrWhiteSpace(_currentCar.VIN) || _currentCar.VIN.Length != 17)
+                errors.AppendLine("Укажите VIN верно!");
+
+            if (_currentCar.YearProduct < 1900 || _currentCar.YearProduct >= DateTime.Now.Year + 1)
+                errors.AppendLine("Укажите Год верно (1900-текущий)!");
+
+            if (string.IsNullOrWhiteSpace(_currentCar.Color))
+                errors.AppendLine("Укажите Цвет!");
+
+            if (ComboClass.SelectedItem == null)
+                errors.AppendLine("Выберите Кузов!");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            var allCars = IbragimovCarDealershipDBEntities.GetContext().Car.Where(p => (p.MarkID == _currentCar.MarkID &&
+                                                                                       p.Model == _currentCar.Model &&
+                                                                                       p.YearProduct == _currentCar.YearProduct &&
+                                                                                       p.Color == _currentCar.Color &&
+                                                                                       p.ClassID == _currentCar.ClassID &&
+                                                                                       p.CarID != _currentCar.CarID &&
+                                                                                       p.VIN != _currentCar.VIN) 
+                                                                                       || (p.VIN == _currentCar.VIN && p.CarID != _currentCar.CarID)).ToList();
+
+
+            if (allCars.Count == 0)
+            {
+                if (_currentCar.CarID == 0)
+                    IbragimovCarDealershipDBEntities.GetContext().Car.Add(_currentCar);
+                try
+                {
+                    IbragimovCarDealershipDBEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена!");
+                    Manager.MainFrame.GoBack();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            else
+                MessageBox.Show("Уже существует похожий тип авто!");
+        }
+
+        private void ChangePictureBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myOpenFileDialog = new OpenFileDialog();
+            if (myOpenFileDialog.ShowDialog() == true)
+            {
+                string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                string imageFolderPath = System.IO.Path.Combine(appDirectory, "image");
+                myOpenFileDialog.InitialDirectory = imageFolderPath;
+
+                string path = myOpenFileDialog.FileName;
+                int index = path.LastIndexOf("image");
+                path = "\\" + path.Substring(index);
+
+                _currentCar.Image = path;
+                LogoImage.Source = new BitmapImage(new Uri(myOpenFileDialog.FileName));
+            }
+        }
+    }
+}
---
+++ IbragimovIlshatCarDealership/CarPage.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IbragimovIlshatCarDealership
+{
+    /// <summary>
+    /// Логика взаимодействия для CarPage.xaml
+    /// </summary>
+    public partial class CarPage : Page
+    {
+        public CarPage()
+        {
+            InitializeComponent();
+
+            var currentCar = IbragimovCarDealershipDBEntities.GetContext().Car.ToList();
+
+            CarListView.ItemsSource = currentCar;
+
+
+            ComboType.SelectedIndex = 0;
+
+
+            UpdateServices();
+        }
+
+        private void UpdateServices()
+        {
+            var currentCar = IbragimovCarDealershipDBEntities.GetContext().Car.ToList();
+
+            if (ComboType.SelectedIndex == 0)
+                currentCar = currentCar.ToList();
+
+            if (ComboType.SelectedIndex == 1)
+                currentCar = currentCar.Where(p => p.Price < 500000).ToList();
+
+            if (ComboType.SelectedIndex == 2)
+                currentCar = currentCar.Where(p => p.Price >= 500000 && p.Price < 1000000).ToList();
+
+            if (ComboType.SelectedIndex == 3)
+                currentCar = currentCar.Where(p => p.Price >= 1000000 && p.Price < 5000000).ToList();
+
+            if (ComboType.SelectedIndex == 4)
+                currentCar = currentCar.Where(p => p.Price >= 5000000 && p.Price < 10000000).ToList();
+
+            if (ComboType.SelectedIndex == 5)
+                currentCar = currentCar.Where(p => p.Price >= 10000000).ToList();
+
+
+            currentCar = currentCar.Where(p => p.MarkString.ToLower().Contains(TBoxSearch.Text.ToLower()) ||
+                                               p.Model.ToLower().Contains(TBoxSearch.Text.ToLower()) ||
+                                               p.YearProduct.ToString().Contains(TBoxSearch.Text.ToLower()) ||
+                                               p.Color.ToLower().Contains(TBoxSearch.Text.ToLower()) ||
+                                               p.ClassString.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+
+            CarListView.ItemsSource = currentCar.ToList();
+
+
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                currentCar = currentCar.OrderByDescending(p => p.Price).ToList();
+            }
+
+            if (RButtonUp.IsChecked.Value)
+            {
+                currentCar = currentCar.OrderBy(p => p.Price).ToList();
+            }
+
+            CarListView.ItemsSource = currentCar;
+
+            tbCount.Text = "Количество записей: " + currentCar.Count.ToString();
+        }
+
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                IbragimovCarDealershipDBEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                CarListView.ItemsSource = IbragimovCarDealershipDBEntities.GetContext().Car.ToList();
+
+                UpdateServices();
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Car));
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var currentCar = (sender as Button).DataContext as Car;
+
+            var currentSale = IbragimovCarDealershipDBEntities.GetContext().Sale.ToList();
+            currentSale = currentSale.Where(p => p.CarID == currentCar.CarID).ToList();
+
+            if (currentSale.Count != 0)
+                MessageBox.Show("Невозможно выполнить удаление, так как существуют продажа");
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        IbragimovCarDealershipDBEntities.GetContext().Car.Remove(currentCar);
+                        IbragimovCarDealershipDBEntities.GetContext().SaveChanges();
+
+                        CarListView.ItemsSource = IbragimovCarDealershipDBEntities.GetContext().Car.ToList();
+
+                        UpdateServices();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
